a22d2861fbc32f391409111b854208a1e57beab3
dmouraoliveira77 <88945764+MackMarcos@users.noreply.github.com>
2021-09-05 00:32:08 -0300

    Update zapbot.py

--- zapbot.py
+++ zapbot.py
@@ -5,7 +5,7 @@ import time
 class WhatsappBot:
     def __init__(self):
         # Parte 1 - A mensagem que você quer enviar
-        self.mensagem = "this is a test"
+        self.mensagem = "https//youtube.com"
         # Parte 2 - Nome dos grupos ou pessoas a quem você deseja enviar a mensagem
         self.grupos_ou_pessoas = ["bot_whatsApp2","Bot_WhatsApp"]
         options = webdriver.ChromeOptions()
@@ -34,4 +34,4 @@ class WhatsappBot:
 bot = WhatsappBot()
 bot.EnviarMensagens()
 
-#<div tabindex="-1" class="_1UWac _1LbR4"><div class="_2vbn4" style="visibility: visible;">Digite uma mensagem</div><div role="textbox" class="_13NKt copyable-text selectable-text" contenteditable="true" data-tab="9" dir="ltr" spellcheck="true"></div></div>
+#<div tabindex="-1" class="_1UWac _1LbR4">
